13bbe15890f3107dc572abaccc6245fe946390fb
na <na@railsServer>
2014-10-04 14:20:04 -0700

    On branch master  Corrected some diff stuff stuck in the code from a merge 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
   resources :workouts
 
   resources :exercises
 
->>>>>>> develop
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
